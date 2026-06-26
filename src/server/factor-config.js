1303754fce28ded0da96959123b4df384dffc176
Samyak K Baid <105852196+samyakbaid@users.noreply.github.com>
2026-06-26 18:58:26 +0530

    Revert "Point universe all to new file_6_all_labels (1).csv and update mapping"
    This reverts commit 61081d26decc65e7459cf7bd3c150b41dbd7ad65.

--- src/server/factor-config.js
+++ src/server/factor-config.js
@@ -57,7 +57,7 @@ export const BENCHMARK_OPTIONS = {
 };
 
 export const UNIVERSE_FILES = {
-  all: "file_6_all_labels (1).csv",
+  all: "Data/Updated_Factor_Data/total_universe/21_stock_level_monthly.csv",
   top500: "Data/Updated_Factor_Data/stock_files/21_500stock_level_monthly.csv",
   top300: "Data/Updated_Factor_Data/stock_files/21_300stock_level_monthly.csv",
 };
